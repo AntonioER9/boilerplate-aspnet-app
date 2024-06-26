ba804e4891929c0f909e4e8af33cbee835f7bc10
AntonioER9 <aespinozareyes9@gmail.com>
2024-06-26 13:22:48 -0400

    add controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.EntityFrameworkCore;
 using DTOs;
+using Models;
+using FluentValidation;
 
 namespace Controllers
 {
@@ -8,29 +11,47 @@ namespace Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        // [HttpGet]
-        // public async Task<IEnumerable<ProductDto>> Get() {
+        private DatabaseContext _context;
+        private IValidator<ProductInsertDto> _productInsertValidator;
+        public ProductController(
+            DatabaseContext context,
+            IValidator<ProductInsertDto> productInsertValidator,
+        )
+        {
+            _context = context;
+            _productInsertValidator = productInsertValidator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductDto>> Get() {
             
-        // }
+        }
 
-        // [HttpGet("{id}")]
-        // public async Task<ActionResult<ProductDto>> GetById(int id) {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetById(int id) {
 
-        // }
+        }
 
-        // [HttpPost]
-        // public async Task<ActionResult<ProductDto>> Add(ProductInsertDto productInsertDto) {
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> Add(ProductInsertDto productInsertDto) 
+        {
+            var validationResult = await _productInsertValidator.ValidateAsync(productInsertDto);
 
-        // }
+            if(!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Erros);
+            }
 
-        // [HttpPut("{id}")]
-        // public async Task<ActionResult<ProductDto>> Update(int id, ProductUpdateDto productUpdateDto) {
+        }
 
-        // }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ProductDto>> Update(int id, ProductUpdateDto productUpdateDto) {
 
-        // [HttpDelete("{id}")]
-        // public async Task<ActionResult> Delete(int id) {
+        }
 
-        // }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id) {
+
+        }
     }
 }
